173f5c30276120eaf273c282f2bb309c22d4449d
ByteRatter Official <271485249+ByteRatter@users.noreply.github.com>
2026-04-28 00:29:32 +0600

    Add rankify.js for domain configuration

---
+++ domains/rankify.js
@@ -0,0 +1,9 @@
+module.exports = {
+  owner: {
+    user: 'ByteRatter',
+    email: 'rafsanulislamrazeen@gmail.com', // Replace with your real email
+  },
+  record: {
+    CNAME: 'byteratter.github.io',
+  },
+}
